82a2b2b697cb22ec6b320335c5f29df5f94dbf2a
Hugh Morrison <hmorri32@gmail.com>
2017-10-16 15:47:20 -0600

    ultra crud for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,52 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @categories = Category.order(:title)
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} created!"
+      redirect_to categories_path
+    else
+      @errors = @category.errors.full_messages
+      redirect_to new_category_path
+    end
+  end
+
+  def show
+    @jobs = @category.jobs
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:success] = "#{@category.title} updated!"
+      redirect_to categories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
 end

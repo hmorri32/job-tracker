1d1faf331a2e2375a2b6c2595e9c086c1967e3d2
Hugh Morrison <hmorri32@gmail.com>
2017-10-17 13:06:04 -0600

    add reset css

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'companies#index'
+  root :to => 'companies#index'
 
   resources :companies do
     resources :jobs
   end
 
-  resources :jobs, only: []  do
-    resources :comments, only: [:create]
+  resources :jobs, :only => []  do
+    resources :comments, :only => [:create]
   end
 
   resources :categories

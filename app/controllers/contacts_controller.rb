830eb17c66e76d1244d5b1465c238c33a25e63c9
Hugh Morrison <hmorri32@gmail.com>
2017-10-17 14:11:50 -0600

    ultra crud contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,16 +1,12 @@
 class ContactsController < ApplicationController
-
-  def new
-    @company = Company.find(params[:id])
-  end
+  before_action :set_company, :only => [:new, :create]
 
   def create
-    @company = Company.find(params[:id])
     @jobs    = @company.jobs
     @contact = Contact.new(contact_params)
     @contact.company_id = @company.id
     if @contact.save
-      flash[:success] = "#{contact.name} added!"
+      flash[:success] = "#{@contact.name} added!"
       redirect_to company_path(@contact.company)
     else
       redirect_to company_path(@contact.company)
@@ -21,6 +17,9 @@ class ContactsController < ApplicationController
   end
 
   private
+  def set_company
+    @company = Company.find(params[:company_id])
+  end
 
   def contact_params
     params.require(:contact).permit(:name, :position, :email)
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,7 +3,8 @@ class JobsController < ApplicationController
 
   def index
     @company = Company.find(params[:company_id])
-    @jobs = @company.jobs
+    @jobs    = @company.jobs
+    @contact = Contact.new
   end
 
   def new

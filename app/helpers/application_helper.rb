5a7ae2c6a1039a8946db5e04414019b158833c3e
Hugh Morrison <hmorri32@gmail.com>
2017-10-18 17:48:33 -0600

    require container in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def companies
+    Company.order(:name)
+  end
+
+  def cities
+    Job.order(:city)
+       .distinct
+       .pluck(:city)
+  end
+
+  def categories
+    Category.order(:title)
+  end
 end

88374072d59c04cd582a995250d7056b299a65f7
Hugh Morrison <hmorri32@gmail.com>
2017-10-18 18:58:24 -0600

    make use of helpers for controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  include CommentsHelper
 
   def new
     @job = Job.find(params[:job_id])
@@ -8,13 +9,10 @@ class CommentsController < ApplicationController
     @job     = Job.find(params[:job_id])
     @comment = Comment.new(comment_params)
     @comment.job_id = @job.id
-    if @comment.save
-      flash[:success] = "Comment Created!"
-      redirect_to company_job_path(@comment.job.company, @comment.job)
-    else
-      redirect_to company_job_path(@comment.job.company, @comment.job)
-    end
+    create_comment(@comment)
   end
+
+  private
 
   def comment_params
     params.require(:comment).permit(:content)
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
 module CommentsHelper
+  def create_comment(comment)
+    if @comment.save
+      flash[:success] = "Comment Created!"
+      redirect_to company_job_path(@comment.job.company, @comment.job)
+    else
+      redirect_to company_job_path(@comment.job.company, @comment.job)
+    end
+  end
 end
